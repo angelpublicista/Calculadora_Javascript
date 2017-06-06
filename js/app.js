39adc8d278f1de031b9c7e8446201ff0223c32fb
angelpublicista <angelpublicista.1@gmail.com>
2017-06-05 20:42:23 -0500

    Maximo de caracteres

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 Calculadora = (function(){
   
 //Funcion Tamaño Teclas
-	teclas = document.getElementsByClassName('tecla');
 
-	for (i = 0; i < teclas.length; i++) {
-		teclas[i].onmousedown = function() {
-			this.style.padding='3px';
-		};
+	function tamanoTeclas() {
+		teclas = document.getElementsByClassName('tecla');
+		for (i = 0; i < teclas.length; i++) {
+			teclas[i].onmousedown = function() {
+				this.style.padding='3px';
+			};
 
-		teclas[i].onmouseup = function() {
-			this.style.padding='0';
+			teclas[i].onmouseup = function() {
+				this.style.padding='0';
+			};
 		};
-	}
+	};
+
+	tamanoTeclas();
+
+	
 
 //Accediendo al display
 	display = document.getElementById('display');
@@ -28,33 +34,41 @@ Calculadora = (function(){
 	cero = document.getElementById('0');
 
 	numeros = [uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, cero];
-	
-	function nuevoNumero() {
-			for (i in numeros) {
+
+
+
+	function escribir() {
+		for (i in numeros) {
 			numeros[i].onclick = function() {
 			anterior = display.innerHTML;
 
 			if (anterior == 0) {
 					newNumber = this.getAttribute('alt');
-					newNumber = parseFloat(newNumber);
 					display.innerHTML = newNumber;
 				} else {
 					newNumber = anterior + this.getAttribute('alt');
-					newNumber = parseFloat(newNumber);
 					display.innerHTML = newNumber;
 				};
-				
+
+	//Máximo de 8 caracteres
+
+				if (newNumber.length > 8) {
+					display.innerHTML = anterior;
+					newNumber = newNumber.substring(0,8);
+				};
 			};
 		};
 	};
 
-	nuevoNumero();
+	escribir();
+
 
 //ON/C
 	eliminar = document.getElementById('on');
 	function eliminar_todo() {
-		display.innerHTML = '0';
-	}
+		newNumber = 0;
+		display.innerHTML = newNumber;
+	};
 
 	eliminar.addEventListener('click', eliminar_todo);
 	
@@ -66,7 +80,32 @@ Calculadora = (function(){
 	dividido = document.getElementById('dividido');
 	igual = document.getElementById('igual');
 
+
+/*dividir: function(){
+if(accion == "inicio"){accion = "dividir"}
+if(accion != "dividir"){
+numero1 = resultado
+display.innerHTML = ''
+accion = "dividir"
+}else{
+if(numero1 == ''){
+numero1 = Number(display.innerHTML)
+display.innerHTML = ''
+}else{
+if(display.innerHTML == resultado){
+numero1 = Number(display.innerHTML)
+numero2 = numero2
+}else{
+numero2 = Number(display.innerHTML)
+}
+resultado = numero1 / numero2;
+display.innerHTML = this.validarDisplay(resultado)
+this.agregarOperacion(resultado)
+}
+}
+} */
+
 	
-	
+
 
 }());
